14b0f7a7b2f7569df83af2d45b896154a2d88737
Neil van der merwe <necktro101@gmail.com>
2018-07-27 16:50:22 +0200

    add
    I added all the items and built the clock

--- Group 2 Assignment/Assets/Scripts/Clock.cs
+++ Group 2 Assignment/Assets/Scripts/Clock.cs
@@ -27,9 +27,9 @@ public class Clock : MonoBehaviour
 	void Update ()
     {
         DateTime time = DateTime.Now;
-        hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
-        minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
-        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * hoursToDegrees);
+        minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * minutesToDegrees);
+        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * secondsToDegrees);
      
     }
 }
